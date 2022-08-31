fcb433fcdb6374626a06c7db267a4c1e7a88e181
Sergio <sergioandyp@gmail.com>
2022-08-31 19:47:56 -0300

    LEDMux DRV and test

---
+++ source/LEDMux/LEDMux.c
@@ -0,0 +1,111 @@
+/***************************************************************************//**
+  @file		LEDMux.c
+  @brief    Funciones para manejo de los LEDs mediante MUX
+  @author	Sergio Peralta
+  @date		31 ago. 2022
+ ******************************************************************************/
+
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+
+#include "LEDMux.h"
+#include "gpio.h"
+#include "timer/timer.h"
+
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
+
+#define LED_SEL_PIN_COUNT	2
+
+#define SEL1_PIN	PORTNUM2PIN(PA, 2)
+#define SEL2_PIN	PORTNUM2PIN(PB, 23)
+
+/*******************************************************************************
+ * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
+ ******************************************************************************/
+
+enum LED_SEL_STATES	{LED_OFF, LED1_ON, LED2_ON, LED3_ON};
+
+
+/*******************************************************************************
+ * GLOBAL VARIABLES
+ ******************************************************************************/
+
+tim_id_t timerID;
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+
+void muxWrite(uint8_t num);
+
+
+/*******************************************************************************
+ * ROM CONST VARIABLES WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+
+static const pin_t selPins[] = {SEL1_PIN, SEL2_PIN};
+
+/*******************************************************************************
+ *******************************************************************************
+                        GLOBAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+
+/**
+ * @brief Init Hw for LEDs
+ * @return init succes
+*/
+int LEDMuxInit() {
+	gpioMode(SEL1_PIN, OUTPUT);
+	gpioMode(SEL2_PIN, OUTPUT);
+
+	LEDMuxOff();
+
+	timerID = timerGetId();		// ID Request for timed functions
+
+	return true;
+}
+
+
+/**
+ * @brief Turn on one of the LEDs
+ * @param ledN: The number of LED to turn on [1 - N]
+*/
+void LEDMuxSet(uint8_t ledN) {
+	muxWrite(ledN);
+}
+
+
+/**
+ * @brief Turn on one of the LEDs for the time specified
+ * @param ledN: The number of LED to turn on [1 - N]
+ * @param time: time to show in ms
+*/
+void LEDMuxSetForTime(uint8_t ledN, uint32_t time) {
+	timerStart(timerID, TIMER_MS2TICKS(time), TIM_MODE_SINGLESHOT, LEDMuxOff);
+}
+
+
+/**
+ * @brief Turn off the LEDs
+*/
+void LEDMuxOff() {
+	muxWrite(LED_OFF);
+}
+
+/*******************************************************************************
+ *******************************************************************************
+                        LOCAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+
+void muxWrite(uint8_t num) {
+	for (int i = 0; i < LED_SEL_PIN_COUNT; i++) {
+		gpioWrite(selPins[i], num & 0x1);		// Write last bit
+		num >>= 1;								// Next bit
+	}
+}
+ 
---
+++ source/LEDMux/LEDMux.h
@@ -0,0 +1,48 @@
+/***************************************************************************//**
+  @file     LEDMux.h
+  @brief    Funciones para manejo de los LEDs mediante MUX
+  @author   Sergio Peralta
+  @date		31 ago. 2022
+ ******************************************************************************/
+
+#ifndef _LEDMUX_LEDMUX_H_
+#define _LEDMUX_LEDMUX_H_
+
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+
+#include <stdint.h>
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
+ ******************************************************************************/
+
+/**
+ * @brief Init Hw for LEDs
+ * @return init succes
+*/
+int LEDMuxInit();
+
+/**
+ * @brief Turn on one of the LEDs
+ * @param ledN: The number of LED to turn on [1 - N]
+*/
+void LEDMuxSet(uint8_t ledN);
+
+/**
+ * @brief Turn on one of the LEDs for the time specified
+ * @param ledN: The number of LED to turn on [1 - N]
+ * @param time: time to show in ms
+*/
+void LEDMuxSetForTime(uint8_t ledN, uint32_t time);
+
+/**
+ * @brief Turn off the LEDs
+*/
+void LEDMuxOff();
+
+/*******************************************************************************
+ ******************************************************************************/
+
+#endif // _LEDMUX_LEDMUX_H_
