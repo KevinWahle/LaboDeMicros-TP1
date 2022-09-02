f9fa1d7e4eab716a50f36a4c6ca51d8adc5e4896
Kevin Wahle <kevinwcarp@gmail.com>
2022-09-02 10:50:45 -0300

    Comments added

--- source/magtek/MagtekWrapper.c
+++ source/magtek/MagtekWrapper.c
@@ -1,5 +1,5 @@
 /***************************************************************************//**
-  @file     +MagtekWrapper.h+
+  @file     +MagtekWrapper.c+
   @brief    +Mangetic stripe wrapper for 8 digit Id+
   @author   +Group 5+
  ******************************************************************************/
@@ -7,8 +7,6 @@
 /*******************************************************************************
  * INCLUDE HEADER FILES
  ******************************************************************************/
-
-// +Incluir el header propio (ej: #include "template.h")+
 #include "MagtekWrapper.h"
 #include <string.h>
 #include <stdlib.h>
@@ -29,22 +27,17 @@
  * VARIABLES WITH GLOBAL SCOPE
  ******************************************************************************/
 
-// +ej: unsigned int anio_actual;+
-
 
 /*******************************************************************************
  * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
  ******************************************************************************/
 
-// +ej: static void falta_envido (int);+
 static void new_callback (bool state, const data* data);
 
 
 /*******************************************************************************
  * ROM CONST VARIABLES WITH FILE LEVEL SCOPE
  ******************************************************************************/
-
-// +ej: static const int temperaturas_medias[4] = {23, 26, 24, 29};+
 
 
 /*******************************************************************************
@@ -75,19 +68,16 @@ bool Card2Init(WrapperCardCb funCb){
  ******************************************************************************/
 
 void new_callback (bool state, const data* data){
-	for (int i = 0; i < strlen(data->id); i++) {
-	
-	}
 	if (state==true){
 		if (strlen(data->id)>ID_SIZE){
-			memcpy(newId, data->id+(strlen(data->id)-ID_SIZE)*sizeof(char),ID_SIZE);
+			memcpy(newId, data->id+(strlen(data->id)-ID_SIZE)*sizeof(char),ID_SIZE);		//If ID > N characters, save the last N ones
 		}
 		else{
 			memcpy(newId, data->id,ID_SIZE);
 		}
 		newId[ID_SIZE]='\0';
-		if(strlen(newId)!=ID_SIZE){
-			for (int i = strlen(newId); i < ID_SIZE; i++) {
+		if(strlen(newId)!=ID_SIZE){										//if ID < N characters, complete with '0'
+			for (int i = strlen(newId); i < ID_SIZE; i++) {								
 				newId[i]='0';
 			}
 		}
